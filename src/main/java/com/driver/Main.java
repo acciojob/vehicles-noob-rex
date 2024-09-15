0499dc0c09c6b26097b5f13699b660ae22d2d6d6
pr@bin <99886121+noob-rex@users.noreply.github.com>
2024-09-15 19:16:49 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,15 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        // Testing F1 car
         F1 mercedes = new F1("mercedes", false);
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+
+        // Testing Boat
+        Boat yacht = new Boat("Yacht", 200);
+        System.out.println("Boat Name: " + yacht.getVehicleName());
+        System.out.println("Boat Capacity: " + yacht.getVehicleCapacity());
     }
 }
